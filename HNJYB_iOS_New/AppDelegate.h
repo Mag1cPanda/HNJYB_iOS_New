3c4fb6e19fc2505812b487a19847e2aa80785093
Peter Pan <415391360@qq.com>
2017-10-16 16:59:54 +0800

    Initial Commit

---
+++ HNJYB_iOS_New/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HNJYB_iOS_New
+//
+//  Created by panshen on 2017/10/16.
+//  Copyright © 2017年 panshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
